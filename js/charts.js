55ae1f22c589b57f913ee5b161bf2a54ae6c2d60
Kelli <kelliwolf17@gmail.com>
2021-01-10 19:23:34 -0600

    adding notes to charts.js

--- js/charts.js
+++ js/charts.js
@@ -1,3 +1,5 @@
+// this is not complete, I still need to get Deliverable 2-3 to show up and do Deliverable 4
+
 function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
